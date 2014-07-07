44d18616ee9fd955ffa433e28a7004bae5187245
Kelly Miyashiro <kmiyashiro@users.noreply.github.com>
2014-07-06 23:06:11 -0700

    Organize into methods

--- scraper.py
+++ scraper.py
@@ -10,46 +10,59 @@ import datetime
 # Read in a page
 html = scraperwiki.scrape("http://www.delpopolosf.com/", None, "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; en-us) AppleWebKit/534.27+ (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27")
 
-# Create tree
-root = lxml.html.fromstring(html)
-
-
-schedule = root.cssselect("#weekly_schedule")[0]
-
-# Remove info paragraph
-schedule.cssselect(".first")[0].drop_tree()
-
-# Rest of paragraphs are schedule infos
-days = schedule.cssselect("p")
-
 # Remove non-numbers from day, like "th" from "May 30th"
 non_number = re.compile(r'(\d)[^\d]+')
 
 # Current year, all dates are current year
 current_year = str(datetime.datetime.now().year)
 
+def get_day_elements(html_string):
+  # Create tree
+  root = lxml.html.fromstring(html_string)
+  schedule = root.cssselect("#weekly_schedule")[0]
+
+  # Remove info paragraph
+  schedule.cssselect(".first")[0].drop_tree()
+
+  # Rest of paragraphs are schedule infos
+  return schedule.cssselect("p")
+
+# Get day text "May 30th"
+def get_day_text(day_element):
+  day_text = day.cssselect("strong")[0].text_content()
+  return non_number.sub("\g<1>", day_text) + " " + current_year
+
+def get_time_text(day_element):
+  time_place_text = _get_time_place_text(day_element)
+  return time_place_text.split("@")[0].strip()
+
+def get_date_time_text(day_element):
+  return get_day_text(day_element) + ' ' + get_time_text(day_element)
+
+def get_place_text(day_element):
+  time_place_text = _get_time_place_text(day_element)
+  return time_place_text.split("@")[1].strip()
+
+def get_map_link(day_element):
+  return day.cssselect("a")[0].get("href")
+
+def _get_time_place_text(day_element):
+  return day_element.cssselect("em")[0].text_content()
+
 # Data to save
 data = []
 
+days = get_day_elements(html)
+
 for day in days:
   details = {}
-  # Get day text "May 30th"
-  day_text = day.cssselect("strong")[0].text_content()
-  day_text = non_number.sub("\g<1>", day_text) + " " + current_year
 
-  time_place_text = day.cssselect("em")[0].text_content()
-  time_text = time_place_text.split("@")[0].strip()
-  place_text = time_place_text.split("@")[1].strip()
-
-  map_link = day.cssselect("a")[0].get("href")
-
-  date_time_text = day_text + ' ' + time_text
-
-  details["location"] = place_text
-  details["map_link"] = map_link
+  details["location"] = get_place_text(day)
+  details["map_link"] = get_map_link(day)
 
   # Convert day to date
   try:
+    date_time_text = get_date_time_text(day)
     day_date = datetime.datetime.strptime(date_time_text, "%A, %B %d %Y %I:%M %p")
     details["date"] = day_date.isoformat()
   except ValueError as e:
@@ -58,7 +71,7 @@ for day in days:
   if details.has_key("date"):
     data.append(details)
 
-for datum in data
+for datum in data:
   print datum
 
 # Write out to the sqlite database using scraperwiki library
